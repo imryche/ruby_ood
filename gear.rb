5284026171857a8dbdbf1fc9c5b2c3ca98a42daa
Dmitry Chernyshov <dmitry@razortheory.com>
2018-10-31 07:39:47 +0300

    add Wheel struct with diameter method to Gear class

--- gear.rb
+++ gear.rb
@@ -1,22 +1,23 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
   def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = Wheel.new(rim, tire)
   end
 
   def ratio
     chainring / cog.to_f
   end
 
-  def diameter
-    rim + (tire * 2)
+  def gear_inches
+    ratio * wheel.diameter
   end
 
-  def gear_inches
-    ratio * diameter
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
   end
 end
 
